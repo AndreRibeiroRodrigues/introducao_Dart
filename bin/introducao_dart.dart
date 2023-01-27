1a548afa6d0cb7e2ef3e386df2ac34337aee284d
AndreRRodrigues <83727384+Rayjir@users.noreply.github.com>
2023-01-27 08:36:39 -0300

    First Commit

---
+++ bin/introducao_dart.dart
@@ -0,0 +1,5 @@
+import 'package:introducao_dart/introducao_dart.dart' as introducao_dart;
+
+void main(List<String> arguments) {
+  
+}
---
+++ lib/a_variaveis/a_tipos_variaveis.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Tipos
+  // String, int, double, bool, num
+
+  // Declarações
+  String curso = "Academia do flutter";
+  int accessoEmAnos = 2;
+  double valor = 1196.00;
+  bool turmaAberta = false;
+
+  // Tipos nulos
+  //Todos os tipos podem receber 2 valoeres (NonNull e Null )
+  String? cursoNulo;
+  int? accessoEmAnosN;
+  double? valorN;
+  bool? turmaAbertaN;
+
+  print([
+    curso,
+    accessoEmAnos,
+    valor,
+    turmaAberta,
+    cursoNulo,
+    accessoEmAnosN,
+    valorN,
+    turmaAbertaN
+  ]);
+}
